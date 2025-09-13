83d26567f37155f50e032bc9b09ed20ec7d35cd8
simran verma <vermasimran327@gmail.com>
2025-09-13 21:33:00 +0530

    Delete server  directory

--- server /models/User.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.model('User', new mongoose.Schema({
-    email:{type: String, unique:true},
-    passwordHash:String,
-},
-{timemstamps:true}));
